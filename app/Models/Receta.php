8a22dd6b39e2543c8708ffb908d566d23800e8ae
luckysnchz <calder07@gmail.com>
2022-05-10 21:40:20 -0300

    modificacion Model User y Model Receta

--- app/Models/Receta.php
+++ app/Models/Receta.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Receta extends Model
 {
-    use HasFactory;
+    protected $table = 'recetas';
+    protected $guarded = [];
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    use HasFactory;
+    protected $table = 'users';
+    protected $guarded = [];
 }
